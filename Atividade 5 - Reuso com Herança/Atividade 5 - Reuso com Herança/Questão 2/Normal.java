d1f957e971fe5dc460de2da47f4ae2d302798f28
EronilsonHolanda <127308928+EronilsonHolanda@users.noreply.github.com>
2023-10-24 21:36:26 -0300

    Delete Atividade 5 - Reuso com Herança directory

--- Atividade 5 - Reuso com Herança/Atividade 5 - Reuso com Herança/Questão 2/Normal.java
+++
@@ -1,9 +0,0 @@
-public class Normal extends Ingresso {
-    public Normal(double valor) {
-        super(valor);
-    }
-
-    public void imprimeTipo() {
-        System.out.println("Ingresso Normal");
-    }
-}
