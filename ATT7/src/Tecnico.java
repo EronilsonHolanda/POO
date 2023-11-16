4bb5870e4911ea165b25e4a8d774baa8bfacf807
Eronilson Holanda <127308928+EronilsonHolanda@users.noreply.github.com>
2023-11-16 11:02:29 -0300

    Add files via upload

---
+++ ATT7/src/Tecnico.java
@@ -0,0 +1,13 @@
+public class Tecnico extends Assistente {
+    private double bonusSalarial;
+
+    public Tecnico(String nome, double salario, int numeroMatricula, double bonusSalarial) {
+        super(nome, salario, numeroMatricula);
+        this.bonusSalarial = bonusSalarial;
+    }
+
+    @Override
+    public double ganhoAnual() {
+        return super.ganhoAnual() + bonusSalarial;
+    }
+}
